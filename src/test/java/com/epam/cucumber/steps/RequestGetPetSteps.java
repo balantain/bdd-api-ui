921d01a302a07631a7193294a076862a4dfd5522
Aliaksandr_Dyrykau <aliaksandr_dyrykau@epam.com>
2023-10-09 15:36:08 +0300

    add logging

--- src/test/java/com/epam/cucumber/steps/RequestGetPetSteps.java
+++ src/test/java/com/epam/cucumber/steps/RequestGetPetSteps.java
@@ -8,9 +8,11 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
 import java.util.List;
-
+@Slf4j
 public class RequestGetPetSteps {
     private RequestController requestController;
     private String[] statuses;
@@ -20,15 +22,18 @@ public class RequestGetPetSteps {
     public void getBaseApiConfiguration() {
         RequestSpecification requestSpecification = ConfigurationManager.getConfig(FilterManager.getAllureLoggingFilter());
         requestController = new RequestController(requestSpecification);
+        log.info("Base request specification is created with AllureLoggingFilter");
     }
 
     @And("Pet statuses are:")
     public void setPetStatuses(List<String> statuses) {
         this.statuses = statuses.toArray(new String[0]);
+        log.info("Statuses " + statuses + " are added to params");
     }
 
     @When("User triggers GET request with {string} endpoint")
     public void getPetsByPreparedStatus(String endpoint) {
         response = requestController.getPetByStatusWithEndpoint(endpoint, statuses);
+        log.info("Endpoint " + endpoint + " is triggered with statuses: " + Arrays.toString(statuses));
     }
 }
--- src/test/java/com/epam/cucumber/steps/ResponseGetPetsSteps.java
+++ src/test/java/com/epam/cucumber/steps/ResponseGetPetsSteps.java
@@ -4,8 +4,9 @@ import com.epam.api.models.Pet;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
-
+@Slf4j
 public class ResponseGetPetsSteps {
 
     private final Response response = RequestGetPetSteps.response;
@@ -13,6 +14,7 @@ public class ResponseGetPetsSteps {
     @Then("Response status code is {int}")
     public void validateResponseStatusCode(int statusCode) {
         response.then().statusCode(statusCode);
+        log.info("Validating status code");
     }
 
     @And("Number of pets in response is greater then {int}")
